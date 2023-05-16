24b2461448e1ad4df36bea89b93e5a6558a5b7cb
Gongskie1 <markjosephtiempo0@gmail.com>
2023-05-16 23:55:35 +0800

    fixed

--- PAYROLL123/hr_emp/employee/empEdit/delete.php
+++ PAYROLL123/hr_emp/employee/empEdit/delete.php
@@ -1,15 +1,20 @@
-<?php include 'connect.php'; 
+<?php
+include 'connect.php';
 
 if (isset($_POST['delete'])) {
   $deleteRow = $_POST['delete'];
 
-  $sql = "DELETE FROM addemployee WHERE id=$deleteRow";
-  $sql1 = "DELETE FROM users WHERE user_id=$deleteRow";
 
-if ($conn->query($sql) === TRUE &&$conn->query($sql1) === TRUE) {
-  echo "Record deleted successfully";
-} else {
-  echo "Error deleting record: " . $conn->error;
+  $attendanceSql = "DELETE FROM attendance WHERE empID=$deleteRow";
+  if ($conn->query($attendanceSql)) {
+
+    $employeeSql = "DELETE FROM addemployee WHERE id=$deleteRow";
+    if ($conn->query($employeeSql)) {
+      echo "Record deleted successfully";
+    } else {
+      echo "Error deleting record: " . $conn->error;
+    }
+  } else {
+    echo "Error deleting attendance records: " . $conn->error;
+  }
 }
-}
-?>
